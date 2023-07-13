98e1a4a1274705351ef6f8a40b5e1056ebbecb77
Jeff Dunn <jdunn@womply.com>
2023-07-13 10:54:50 -0700

    Add echo API endpoint

--- myApp.js
+++ myApp.js
@@ -11,23 +11,27 @@ app.use((req, res, next) => {
   const output = `${req.method} ${req.path} - ${req.ip}`;
   console.log(output);
   next();
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
-})
+});
+
+app.get('/json', (req, res) => {
+  const messageStyle = process.env.MESSAGE_STYLE;
+  const message = 'Hello json';
+  res.json({ message: messageStyle === 'uppercase' ? message.toUpperCase() : message });
+});
 
 app.get('/now', (req, res, next) => {
   req.time = new Date().toString();
   next();
 }, (req, res) => {
   res.json({ time: req.time });
-})
+});
 
-app.get('/json', (req, res) => {
-  const messageStyle = process.env.MESSAGE_STYLE;
-  const message = 'Hello json';
-  res.json({ message: messageStyle === 'uppercase' ? message.toUpperCase() : message });
-})
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word });
+});
 
 module.exports = app;
